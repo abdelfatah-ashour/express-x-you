7a68266d7957c68e268f8183ad8073c0b3b559ff
abdelfatah ashour <abdelfatahashour4@gmail.com>
2021-05-26 21:10:28 +0200

    test cart

--- client/src/pages/cart.js
+++ client/src/pages/cart.js
@@ -1,123 +1,143 @@
 import React, { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 import RowCard from '../components/RowCard';
-import axios from '../utilities/axios';
+// import axios from '../utilities/axios';
 import Error from 'next/error';
 import CheckoutStepper from '../components/CheckoutStepper';
 import Style from '../styles/card.module.css';
 
-export default function cart({ cart, error }) {
-  const [totalAmount, setTotalAmount] = useState(0);
-  const [items, setItems] = useState([]);
+export default function cart({ cart, error, cookies, headers }) {
+    console.log('COOKIES   : ', { cookies });
+    console.log('HEADERS   : ', { headers });
+    const [totalAmount, setTotalAmount] = useState(0);
+    const [items, setItems] = useState([]);
 
-  const [checked, setChecked] = useState(false);
+    const [checked, setChecked] = useState(false);
 
-  useEffect(() => {
-    if (cart && cart.length > 0) {
-      const result = cart.map(item => {
-        return {
-          product: item.product._id,
-          qty: item.qty,
-          amount: item.qty * item.product.price,
+    useEffect(() => {
+        if (cart && cart.length > 0) {
+            const result = cart.map(item => {
+                return {
+                    product: item.product._id,
+                    qty: item.qty,
+                    amount: item.qty * item.product.price,
+                };
+            });
+
+            const total = result.reduce((acc, cur) => {
+                return acc + cur.amount;
+            }, 0);
+            setItems(result);
+            setTotalAmount(total);
+        }
+        return () => {
+            return;
         };
-      });
+    }, []);
 
-      const total = result.reduce((acc, cur) => {
-        return acc + cur.amount;
-      }, 0);
-      setItems(result);
-      setTotalAmount(total);
-    }
-    return () => {
-      return;
-    };
-  }, []);
+    // reduce total amount
 
-  // reduce total amount
+    return (
+        <Layout title="CART">
+            <main className="container">
+                {cart &&
+                    !checked &&
+                    cart.length > 0 &&
+                    cart.map((item, i) => {
+                        return (
+                            <RowCard cart={item} key={i} cartId={cart[0]._id} />
+                        );
+                    })}
 
-  return (
-    <Layout title="CART">
-      <main className="container">
-        {cart &&
-          !checked &&
-          cart.length > 0 &&
-          cart.map((item, i) => {
-            return <RowCard cart={item} key={i} cartId={cart[0]._id} />;
-          })}
+                {(!cart || cart.length === 0) && (
+                    <div className="w-100 my-auto">
+                        <div
+                            className="alert alert-secondary text-center"
+                            role="alert">
+                            your cart is empty 📪
+                        </div>
+                    </div>
+                )}
 
-        {(!cart || cart.length === 0) && (
-          <div className="w-100 my-auto">
-            <div className="alert alert-secondary text-center" role="alert">
-              your cart is empty 📪
-            </div>
-          </div>
-        )}
+                {items.length > 0 && (
+                    <div
+                        className={
+                            Style.wrapperCheckout +
+                            ' w-100 d-flex justify-content-end'
+                        }>
+                        <div className="w-25 d-flex flex-column justify-content-evenly">
+                            <span>
+                                total amount : <strong>{totalAmount}</strong>$
+                            </span>
+                            <span>payment with : Paypal</span>
+                        </div>
+                        <div className="w-25 d-flex justify-content-evenly align-items-center">
+                            {!checked ? (
+                                <button
+                                    className="btn btn-warning"
+                                    onClick={() => setChecked(true)}>
+                                    checkout
+                                </button>
+                            ) : (
+                                <CheckoutStepper
+                                    setChecked={setChecked}
+                                    items={items}
+                                    amount={totalAmount}
+                                />
+                            )}
+                        </div>
+                    </div>
+                )}
 
-        {items.length > 0 && (
-          <div
-            className={
-              Style.wrapperCheckout + ' w-100 d-flex justify-content-end'
-            }>
-            <div className="w-25 d-flex flex-column justify-content-evenly">
-              <span>
-                total amount : <strong>{totalAmount}</strong>$
-              </span>
-              <span>payment with : Paypal</span>
-            </div>
-            <div className="w-25 d-flex justify-content-evenly align-items-center">
-              {!checked ? (
-                <button
-                  className="btn btn-warning"
-                  onClick={() => setChecked(true)}>
-                  checkout
-                </button>
-              ) : (
-                <CheckoutStepper
-                  setChecked={setChecked}
-                  items={items}
-                  amount={totalAmount}
-                />
-              )}
-            </div>
-          </div>
-        )}
-
-        {error && <Error statusCode={500} title={'🥱 Something Went Wrong!'} />}
-      </main>
-    </Layout>
-  );
+                {error && (
+                    <Error
+                        statusCode={500}
+                        title={'🥱 Something Went Wrong!'}
+                    />
+                )}
+            </main>
+        </Layout>
+    );
 }
 
 export async function getServerSideProps(ctx) {
-  if (!ctx.req.cookies.auth) {
+    // if (!ctx.req.cookies.auth) {
+    //   return {
+    //     redirect: {
+    //       destination: '/login',
+    //       permanent: false,
+    //     },
+    //   };
+    // } else {
+    //   return await axios
+    //     .get('/api/cart', {
+    //       headers: {
+    //         authorization: ctx.req.cookies.auth,
+    //       },
+    //     })
+    //     .then(({ data }) => {
+    //       return {
+    //         props: {
+    //           cart: data.message,
+    //           error: null,
+    //         },
+    //       };
+    //     })
+    //     .catch(() => {
+    //       return {
+    //         props: {
+    //           cart: null,
+    //           error: true,
+    //         },
+    //       };
+    //     });
+    // }
     return {
-      redirect: {
-        destination: '/login',
-        permanent: false,
-      },
-    };
-  } else {
-    return await axios
-      .get('/api/cart', {
-        headers: {
-          authorization: ctx.req.cookies.auth,
-        },
-      })
-      .then(({ data }) => {
-        return {
-          props: {
-            cart: data.message,
-            error: null,
-          },
-        };
-      })
-      .catch(() => {
-        return {
-          props: {
+        props: {
             cart: null,
-            error: true,
-          },
-        };
-      });
-  }
+            error: null,
+            cookies: ctx.req.cookies,
+            headers: ctx.req.headers,
+        },
+    };
 }
