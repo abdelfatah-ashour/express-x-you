bcf151c14474368cbc3df6f62bce0a4410f973b9
abdelfatah ashour <abdelfatahashour4@gmail.com>
2021-07-10 14:04:30 +0200

    UPDATE add new api url

--- client/src/utilities/KEYS.js
+++ client/src/utilities/KEYS.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // API: "https://express-x-you.herokuapp.comm",
-  API: "http://localhost:9000",
+  API: "https://express-x-you.herokuapp.com",
+  // API: "http://localhost:9000",
   PAYPAL_CLIENT_ID:
     "AT-0ZYYel-c5CGYqsOhX9a_NDWSCkGrO1p-qTeJ10IFp3fYaMH40sa55Cy50p9pbMCktTdf_190M_gmf",
 };
